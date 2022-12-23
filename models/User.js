468db99e8d5a6fa71abf1e6700389cee1c275e6e
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-12-22 20:05:03 -0500

    import dependencies nad create schema with data

---
+++ models/User.js
@@ -0,0 +1,48 @@
+// import the dependencies
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+// create the schema with this data
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
